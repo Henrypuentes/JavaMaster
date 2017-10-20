a0475b48bca4814980d295085586e3e29606e198
HenryPuentesG <HenryPuentesG@HenryPuentes>
2017-10-19 23:00:01 -0500

    Se incluye Modulo 8 Creacion de Clases

---
+++ 08_3901CreacionClasesObjetos/src/Personas/persona.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Personas;
+
+/**
+ *
+ * @author HenryPuentesG
+ */
+public class persona {
+    
+       //Atributos de la clase
+    
+    String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+    
+    //Metodos de la clase
+    public void desplegarNombre(){
+    
+        System.out.println("Nombre : " + nombre);
+        System.out.println("Apellido Paterno : " + apellidoPaterno);
+        System.out.println("Apellido Materno :" + apellidoMaterno);
+    }
+    
+    
+}
